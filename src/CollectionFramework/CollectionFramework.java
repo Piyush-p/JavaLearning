cdb1b1f340c346d606cf23bc7ae13aad3243a82f
piyushpotfore <ppotfore@gmail.com>
2026-02-21 11:01:08 +0530

    Class for array limitation example

---
+++ src/CollectionFramework/CollectionFramework.java
@@ -0,0 +1,17 @@
+package CollectionFramework;
+
+public class CollectionFramework {
+	
+	public static void main(String[] args)
+	{
+	
+	String[] empName=new String[3];
+	
+	empName[0]="Piyush";
+	empName[1]="Sai";
+	empName[2]="Krishna";
+
+	System.out.println(empName.length);
+}
+
+}
