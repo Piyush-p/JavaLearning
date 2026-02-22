1c9aeceb350308f7407775f56b47df08f58516f8
piyushpotfore <ppotfore@gmail.com>
2026-02-22 10:39:48 +0530

    Updated collection

--- src/CollectionFramework/CollectionFramework.java
+++ src/CollectionFramework/CollectionFramework.java
@@ -12,6 +12,12 @@ public class CollectionFramework {
 	empName[2]="Krishna";
 
 	System.out.println(empName.length);
+	System.out.print(empName[1]);
+	
+	//Array are fixed in size
+	//Memory will be allocated very starting of the array creation
+	//Similar type of data can be stored
+	//no modification can be done
 }
 
 }
