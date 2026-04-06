fd937084efd397488b1eafff4ddaba02e202cd9a
piyushpotfore <ppotfore@gmail.com>
2026-04-06 08:24:38 +0530

    Added first assignment for Bharath class

---
+++ src/Assignments/ArrayAverage.java
@@ -0,0 +1,22 @@
+package Assignments;
+
+public class ArrayAverage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*Create an array of 10 numbers (any 10 numbers) and print out the Average of 5th and
+		6th Value.*/
+		
+		int[] numbers= {2,4,6,8,10,12,30,21,32,18};
+		double avg;
+		
+		
+			avg=(numbers[4]+numbers[7])/2.0;
+			System.out.println(avg);
+			System.out.println(numbers[0]);
+		
+
+	}
+
+}
